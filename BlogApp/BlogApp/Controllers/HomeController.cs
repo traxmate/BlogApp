aa4857343eb4529506ca2ed2af579fc9a0477651
Emil Asterbäck <itraxmate@hotmail.com>
2015-12-07 11:03:02 +0100

    Test

--- BlogApp/BlogApp/Controllers/HomeController.cs
+++ BlogApp/BlogApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BlogApp.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			ViewBag.Message = "Your application description page.!";
 
 			return View();
 		}
